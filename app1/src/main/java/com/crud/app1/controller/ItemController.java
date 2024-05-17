6761059fe9f61f637c230af01d8037db13169eca
Marty <jmjmjm373@gmail.com>
2024-05-18 05:07:36 +0800

    Added location for the response when creating a new item.

--- app1/src/main/java/com/crud/app1/controller/ItemController.java
+++ app1/src/main/java/com/crud/app1/controller/ItemController.java
@@ -1,11 +1,11 @@
 package com.crud.app1.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.crud.app1.model.Item;
 import com.crud.app1.service.ItemService;
@@ -54,7 +55,15 @@ public class ItemController {
 		logger.info("POST request received to create item: {}", item);
 		System.out.println(item.getId());
 		Item savedItem = itemService.save(item);
-		return ResponseEntity.status(HttpStatus.CREATED).body(savedItem);
+		
+
+	    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+	            .path("/{id}")
+	            .buildAndExpand(savedItem.getId())
+	            .toUri();
+
+	    return ResponseEntity.created(location).body(savedItem);
+
 
 	}
 
